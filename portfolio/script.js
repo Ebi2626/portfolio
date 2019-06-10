ac1e72a97200612b47a9872054905722b873b110
Edwin <46379855+Ebi2626@users.noreply.github.com>
2019-06-10 19:20:26 +0200

    Final version of my portfolio. It needs only testing by other people.

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,104 +1,175 @@
-$(document).ready(function Menu(){
-	$("#menuToggleBar").hide();
-	var mainMenu = '<section id="menuSection" class="menuSection row justify-content-center text-center" style="height: 100vh;"><div class="col-3 text-center" id="leftMenu"><p class="menuText"><a id="menuAboutMe">About Me</a></p></div><div class="col-6" id="matrixCode"><canvas id="canvas" style="width:100%; height:100vh; font-size: 32px;"></canvas></div><div class="col-3 text-center" id="rightMenu"><p class="menuText"><a id="menuProjects">My projects</a></p></div></section>'
-	var header = '<h1 class="mainHeaderText display-1 col-8 offset-2 col-md-10 offset-md-1 col-sm-10 offset-sm-1 col-xs-12 offset-xs-0">Edwin Harmata</h1><h1 class="mainHeaderText display-1">Web Developer</h1>'
-	var aboutMe = ''
+(function ($) {
+// Variables necessery to intro animation
+const logo = '<div class="mainLogoBox"><img class="mainLogo mx-auto" src="logo.png" style="position: relative;"></div>';
+const header = '<div class="mainDisplay"><h1 class="mainHeaderText display-4">Edwin Harmata<br>Web Developer</h1></div>';
+const height = $(window).outerHeight(true) - $("nav").outerHeight(true);
+const html = '<p id="html">HTML (<b>H</b>yper <b>T</b>ext <b>M</b>arkup <b>L</b>anguage) is the standard markup language for creating Web pages</p>'
+const css = '<p id="css">CSS (<b>C</b>ascading <b>S</b>tyle <b>S</b>heets) is a language that describes the style of an HTML document.</p>'
+const rwd = '<p id="rwd">RWD (<b>R</b>esponsive <b>W</b>eb <b>D</b>esign) is an approach to web design that makes web pages render well on a variety of devices and window or screen sizes.</p>'
+const bootstrap = '<p id="bootstrap"><b>Bootstrap</b> is the most popular HTML, CSS, and JavaScript framework for developing responsive, mobile-first websites.</p>'
+const jquery = '<p id="jq"><b>jQuery</b> is a lightweight, "write less, do more", JavaScript library. </p>'
+// First part intro animation - displaying text
+$(document).ready(function(){
+	$("body").height(height);
+	$("nav").hide();
+	$("img").hide();
+	$("ul").hide();
+	$("main").hide();
+	$("footer").hide();
+	$("header").append(header).hide().fadeIn(2000).addClass("align-self-center");
+	});
+// Second part intro animation - displaying everything
+$(document).ready(function(){
+	$(".mainDisplay").append(logo);
+	$("img").fadeOut(0);
 	setTimeout(function(){
-		$("header").append(header).hide().fadeIn(2500).addClass("align-self-center");
-		$(".welcomeSection").css("box-shadow","0px 0px 3vw inset black", 2000);
-		}, 500);
+		$("img").fadeIn(1500);
+	},1500);
 	setTimeout(function(){
-		$("#menuToggleBar").fadeIn(2000);
-	}, 3000);
-
-	if ($("#backgroundPhoto").height() < $(window).height()){
-		$("#backgroundPhoto").attr("style","height: 100vh;");
-	};
-
-	if ($(window).resize()){
-		if ($("#backgroundPhoto").height() < $(window).height()){
-		$("#backgroundPhoto").attr("style","height: 100vh;");
-	};
-	};
-//Funkcja zmiany menu
-$("#menuToggleBar").click(function(){
-	$(".welcomeSection").remove();
-	$("body").after(mainMenu);
-		var canvas = document.getElementById('canvas');
-		var ctx = canvas.getContext('2d');
-		var t = text();
-		var logo = document.getElementById('logo');
-		var lines = [];
-		window.setInterval(draw, 110);
-
-		function draw() {
-		  if (Math.floor(Math.random() * 2) === 0 && lines.length < 100) {
-		    lines.push(new textLine());
-		  }
-		  ctx.clearRect(0, 0, canvas.width, canvas.height);
-		  lines.forEach(function(tl) {
-
-		    ctx.drawImage(tl.text, tl.posX, tl.animate(), 20, 1000);
-		  });
-		  ctx.drawImage(logo, 100, 155, 400, 70);
-		}
-		function textLine() {
-		  this.text = t;
-		  this.posX = (function() {
-		    return Math.floor(Math.random() * canvas.width);
-		  })();
-		  this.offsetY = -1000;
-		  this.animate = function() {
-		    if (this.offsetY >= 0) {
-		      this.offsetY = -1000;
-		    }
-		    this.offsetY += 10;
-		    return this.offsetY;
-		  };
-		}
-		function text() {
-		  var offscreenCanvas = document.createElement('canvas');
-		  offscreenCanvas.width = "30";
-		  offscreenCanvas.height = "1000";
-		  offscreenCanvas.style.display = "none";
-		  document.body.appendChild(offscreenCanvas);
-		  var octx = offscreenCanvas.getContext('2d');
-		  octx.textAlign = "center";
-		  octx.shadowColor = "lightgreen";
-		  octx.shadowOffsetX = 2;
-		  octx.shadowOffsetY = -5;
-		  octx.shadowBlur = 1;
-		  octx.fillStyle = 'darkgreen';
-		  octx.textAlign = "left";
-		  var step = 10;
-		  for (i = 0; i < 100; i++) {
-		    var charCode = 0;
-		    while (charCode < 60) {
-		      charCode = Math.floor(Math.random() * 100);
-		    }
-		    octx.fillText(String.fromCharCode(charCode), 0, step);
-		    step += 10;
-		  }
-		  return offscreenCanvas;
-		};
-		$("#menuToggleBar").hide();
-//Koniec funkcji włączającej menu -> przejście do konkretnych sekcji
-
-//Sekcja O mnie
-		$("#leftMenu").click(function(){
-			$("#menuSection").replaceWith();
-		});
-// Sekcja Moje projekty
-		$("#rightMenu").click(function(){
-			$("#menuSection").remove();
-		});
-		});
-
-
-
-		
+		$(".mainDisplay").slideUp(1000);
+	},3000);
+	setTimeout(function(){
+		$(".mainLogoBox").slideUp(1000);
+	},3000);
 });
+$(document).ready(function(){
+	setTimeout(function(){
+		$("img").fadeIn(500);
+	}, 4000);
+	setTimeout(function(){
+	$("nav").slideDown(500);
+	}, 5500);
+	setTimeout(function(){
+	$("ul").slideDown(500);
+	$("main").fadeIn(500);
+	$("footer").fadeIn(500);
+	}, 6000);
+});
+// Animation of text below the animated circle with technology icons
+$(document).ready(function(){
+	$("#si1").hover(function(){
+		$("#changingText").append(html);
+		},
+		function(){
+			$("#html").remove();
+		});
+	$("#si2").hover(function(){
+		$("#changingText").append(css);
+		},
+		function(){
+			$("#css").remove();
+		});
+	$("#si3").hover(function(){
+		$("#changingText").append(rwd);
+		},
+		function(){
+			$("#rwd").remove();
+		});
+	$("#si4").hover(function(){
+		$("#changingText").append(bootstrap);
+		},
+		function(){
+			$("#bootstrap").remove();
+		});
+	$("#si5").hover(function(){
+		$("#changingText").append(jquery);
+		},
+		function(){
+			$("#jq").remove();
+		});
+	});
+// Script for displaying gifs in myProjects page
+		$(document).ready(function(){
+		var curtain = '<div class="curtain"></div>';
+		var displaySurvey = '<div class="gifDisplay" id="gifPlace"><img class="gif" src="survey.gif"></div>';
+		var displayjQuery = '<div class="gifDisplay" id="gifPlace"><img class="gif"src="jQuery.gif"></div>'; 
+		var displayGame = '<div class="gifDisplay" id="gifPlace"><img class="gif"src="game.gif"></div>'; 
+		var displayPage = '<div class="gifDisplay" id="gifPlace"><img class="gif"src="otg.gif"></div>';
 
+			$("#survey").click(function Survey(){
+				setTimeout(function(){
+					$("#wholeProject").append(displaySurvey);
+			},300);
+				$("#quote").addClass(".opc-off");
+				$(displaySurvey).ready(function(){
+					$("*").click(function(){
+						$("#gifPlace").remove();
+						$("#quote").removeClass(".opc-off");
+					});
+				});
+			});
 
+			$("#jquery").click(function jQuery(){
+				setTimeout(function(){
+					$("#wholeProject").append(displayjQuery);
+			},300);
+				$(".quote").css('opacity', '0.3');
+				$(displayjQuery).ready(function(){
+					$("*").click(function(){
+						$("#gifPlace").remove();
+						$(".quote").css('opacity', '1');
+					});
+				});
+			});
+				
+			$("#game").click(function Game(){
+				setTimeout(function(){
+					$("#wholeProject").append(displayGame);
+			},300);
+				$(".quote").css('opacity', '0.3');
+				$(displayGame).ready(function(){
+					$("*").click(function(){
+						$("#gifPlace").remove();
+						$(".quote").css('opacity', '1');
+					});
+				});
+			});
+	
+			$("#otg").click(function OTG(){
+				setTimeout(function(){
+					$("#wholeProject").append(displayPage);
+			},300);
+				$(".quote").css('opacity', '0.3');
+				$(displayPage).ready(function(){
+					$("*").click(function(){
+						$("#gifPlace").remove();
+						$(".quote").css('opacity', '1');
+					});
+				});
+			});
+		});
+// Script to removing or adding feather img in contact
+			$(document).ready(function(){
+				if ($(window).outerWidth(true) < 992){
+							$("#feather").remove();
+				};
+			});
+		$(document).ready(function(){
+			$(window).resize(function(){
+				if ($(window).outerWidth(true) < 992){
+					$("#feather").remove();
+				};
+			});
+		});
 
-
+		$(document).ready(function(){
+			$(window).resize(function(){
+				if ($("#feather").length > 0){
+					$("#feather").css('opacity', '0.3');
+				} else if ($(window).outerWidth(true) > 992){
+						$("#form").after('<div class="offset-1 offset-md-0 col-4 pl-4" id="feather"><img src="feather.png" alt="feather" class="feather pl-4"></div>');
+					};
+			});
+		});
+// Fixing bootstrap menu with automatic hiding after choosing its one element
+				$(document).ready(function(){
+					$(".nav-link").click(function(){
+						setTimeout(function(){
+							$("#collapsibleNavbar").toggle(300);
+						}, 300);
+					});
+					$("button").click(function(){
+						$("#collapsibleNavbar").toggle(300);
+					});
+				});
+})(jQuery);
